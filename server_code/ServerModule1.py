1c4c4312d4cfd231fd9ff96e528c39126ebeb852
easy1170@gmail.com <easy1170@gmail.com>
2024-08-13 07:50:20 +0000

    Created Start; Edited settings and ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -3,6 +3,7 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
 from datetime import datetime
+import json
 # This is a server module. It runs on the Anvil server,
 # rather than in the user's browser.
 #
@@ -14,10 +15,7 @@ from datetime import datetime
 def get_dv_list():
   return app_tables.dev_tbl.search()
   
-@anvil.server.route("/dv/list")
-def get_user_list():
-  return app_tables.dev_tbl.search()
-  
+
 @anvil.server.callable
 def update_dv(dv, dv_dict):
   # check that the entry given is really a row in the ‘entries’ table
@@ -36,3 +34,16 @@ def delete_dv(dv):
     dv.delete()
   else:
     raise Exception("DV does not exist")
+    
+@anvil.server.callable
+def init_db():
+  dev_list =  app_tables.dev_tbl.search()
+  name_list = [r['name'] for r in dev_list]
+  print(name_list)
+  if (name_list == []):
+    app_tables.dev_tbl.add_row(name = "CCTV-B2-10", ip = "192.168.0.10", snap_url = "http://192.168.0.10/snap/snap.jpg", updated_at=datetime.strptime("2024-08-01 00:00:00", "%Y-%m-%d %H:%M:%S"), feature_url="http://svc.parkingai.kr:23000/uploads/202408/10/region/CCTV-B2-10_20240810164523_959239-1.jpg")
+    app_tables.dev_tbl.add_row(name = "CCTV-B2-11", ip = "192.168.0.11", snap_url = "http://192.168.0.11/snap/snap.jpg", updated_at = datetime.strptime("2024-08-01 00:00:00", "%Y-%m-%d %H:%M:%S"), feature_url = "http://svc.parkingai.kr:23000/uploads/202408/09/region/CCTV-B2-10_20240809200323_262735-1.jpg")
+    print("empty table. added 2 rows")
+  else: 
+    
+    print("filled table:" + json.dumps(name_list))
